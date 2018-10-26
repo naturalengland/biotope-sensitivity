8768e30b444c2fb14a34e001d65363bf386a58e4
Philip Haupt <philip.haupt@gmail.com>
2018-10-26 17:27:13 +0100

    This code should replace the previous MinMax sens code It is currently 10b, and should replce the 10 in due course)

--- scripts/10b_MinMax_sbgr_BAP.R
+++ scripts/10b_MinMax_sbgr_BAP.R
@@ -22,19 +22,25 @@ library(tidyverse)
 
 # Active code: in double list
 sbgr.BAP.min.max.sens <- xap.ls %>%
-        llply(function(x){
+        llply(function(x){ # splits by activity
                 x %>%
-                        llply(function(y){
-                                max.sens.test <- as.tibble(y) %>% 
+                        llply(function(y){ #splits by sub_biogeoregion
+                                min.max.sens <- as.tibble(y) %>% 
                                         dplyr::group_by(eunis.code.gis, PressureCode) %>%
                                         dplyr::mutate(max.sens = max(rank.value), # maximum sensitivity value, done using mutate to preserve the "eunis.match.assessed" column
                                                       min.sens = min(rank.value[rank.value > 3]),
                                                       min.sens.na = min(rank.value)) %>% # minimum sensitity value
-                                        slice(1) # keeps only the top value /selects row by position, done to preserve eunis.match.assessed code
-                        }, .progress = "text")
+                                        slice(1) %>%# keeps only the top value /selects row by position, done to preserve eunis.match.assessed code
+                                        arrange(PressureCode)
+                        })
+        }, .progress = "text") %>% #now we can reshape the data as follows:
+        llply(function(x){#splits by activity, and returns a list split by activity - becuase each activity will be mapped separately
+                x %>%
+                        ldply(function(y){ #splits list by sub_biogeoregion - and returns a dataframe (per activity - see above), becuase we want to match the senstivities to GIS in one go for all biogeoregions (per activity)
+                                y
+                        })
         }, .progress = "text")
 
-#house keeping
-rm(xap.ls,sbgr.bap)
 
+rm(xap.ls)
 
