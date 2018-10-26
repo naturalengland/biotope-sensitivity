e925f72dff6716daf993bb33ca2fa05edd853f5f
Philip Haupt <philip.haupt@gmail.com>
2018-10-26 15:13:19 +0100

    joining sensitivity table to GI and test code developed for saving to SQLITE db

--- scripts/11_GIS_sbgr_BAP_minmax.R
+++ scripts/11_GIS_sbgr_BAP_minmax.R
@@ -1,28 +1,65 @@
 #link the sbgr.BAP.min.max.sens to the GIS
+library(RSQLite)
+library(plyr)
+library(DBI)
+library(tidyverse)
+library(dbplyr)
 
-str(hab.types)
+# connect to the sqlite file
+orig.d <- getwd()
+scratch.d <- "F:/scratch"
+setwd(scratch.d)
+con.gis = DBI::dbConnect(RSQLite::SQLite(), "Phil_Fish_project_Input_Polys_WGS84_Internal_BGR.sqlite")
+src_dbi(con.gis) #show connection details, and list all tables
+
+# get a list of all tables
+dbListTables(con.gis) #phil_fish_project_input_polys_wgs84_internal_bgr
+#test code
+#str(hab.types)
+#str(sbgr.BAP.min.max.sens[[1]])
+#sbgr.hab.gis <- left_join(hab.types,sbgr.BAP.min.max.sens[[1]], 
+#                          by = c("bgr_subreg_id" = "sbgr", "hab.1" = "eunis.code.gis"))# e.g. composite join: left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
+
+con.gis = DBI::dbConnect(RSQLite::SQLite(), "Phil_Fish_project_Input_Polys_WGS84_Internal_BGR.sqlite")
+
+# List tables in your database
+dbListTables(con.gis)
+
+# List columns in a table
+dbListFields(con.gis, "phil_fish_project_input_polys_wgs84_internal_bgr")
 
 
-sbgr.hab.gis <- hab.types %>%
-        unite(sbgr.hab, bgr_subreg_id, hab.1, remove = FALSE)
-names(sbgr.hab.gis)
-unique(sbgr.hab.gis$sbgr.hab)
+
+#make a newSQL lite table
+dbSendQuery(conn=con.gis,
+            "CREATE TABLE test
+            (Date DATETIME,
+            Station TEXT,
+            Snowline TEXT,
+            PRIMARY KEY (Date, Station))
+            ")
 
 
-tmp <- sbgr.BAP.min.max.sens[[1]][[1]]
-names(tmp)
-act <- names(sbgr.BAP.min.max.sens)
-sbgrs <- names(sbgr.BAP.min.max.sens[[1]])
 
 
 
 
-tmp <- sbgr.BAP.min.max.sens %>%
-        ldply(function(x){
-                x %>%
-                        ldply(function(y){
-                                y
-                        })
-                names(x)
-        })
+
+
+
+
+
+
+
+dbWriteTable(conn=con.gis, name="phil_fish_project_input_polys_wgs84_internal_bgr", hab.types,sbgr.BAP.min.max.sens[[1]], row.names=F)
+
+act.sbgr.bps.gis <- sbgr.BAP.min.max.sens %>%
+        llply(function(x){
+                sbgr.hab.gis <- left_join(hab.types,sbgr.BAP.min.max.sens[[1]], 
+                                          by = c("bgr_subreg_id" = "sbgr", "hab.1" = "eunis.code.gis"))# e.g. composite join: left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
+                
+        }, .progress = "text")
+
+
+
 
