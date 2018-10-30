f9d4c6172ef95a8a3ab117aad7926ba320b10ac5
Philip Haupt <philip.haupt@gmail.com>
2018-10-30 13:10:04 +0000

    add filter to remove both <NA> and "NA" values

--- scripts/08b_sbgr_BAP.R
+++ scripts/08b_sbgr_BAP.R
@@ -3,9 +3,6 @@ library(plyr)
 library(dplyr)
 library(magrittr)
 
-
-                
-                
 xap.ls <- act.press.list.2 %>% 
         plyr::llply(function(y){
                         tidy.p <- y# %>% 
@@ -14,14 +11,20 @@ xap.ls <- act.press.list.2 %>%
                         sbgr.bap <- sbgr.matched.btpt.w.rpl %>% 
                                 plyr::llply(function(x) { 
                                         
+                                        #x.df <- x %>% dplyr::select(eunis.code.gis, sbgr, 1:14, 19:354) %>%
+                                        #        tidyr::gather("A5.22":"A5.7211",key = "eunis.assessed",value = "eunis.gis") %>%
+                                        #        filter(eunis.gis != "<NA>") %>%
+                                        #        select(sbgr, eunis.code.gis, eunis.match.assessed = eunis.assessed) %>%
+                                        #        arrange(sbgr, eunis.code.gis,eunis.match.assessed)
+                                        
                                         x.df <- x %>% dplyr::select(eunis.code.gis, sbgr, 1:14, 19:354) %>%
                                                 tidyr::gather("A5.22":"A5.7211",key = "eunis.assessed",value = "eunis.gis") %>%
-                                                filter(eunis.gis != "<NA>") %>%
+                                                filter(eunis.gis != "<NA>" | eunis.gis != "NA") %>%
                                                 select(sbgr, eunis.code.gis, eunis.match.assessed = eunis.assessed) %>%
                                                 arrange(sbgr, eunis.code.gis,eunis.match.assessed)
                         
                         
-                        xp.df <- right_join(x.df, tidy.p, by = c("eunis.match.assessed" = "EUNISCode"))
+                                        xp.df <- right_join(x.df, tidy.p, by = c("eunis.match.assessed" = "EUNISCode"))
                                 
         })
                 
