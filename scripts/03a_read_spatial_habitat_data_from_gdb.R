f9c2c7dabad334f4a78fb2e77dfbaec4743f2cc4
Philip Haupt <philip.haupt@gmail.com>
2018-11-08 17:07:53 +0000

    #clean up try read from network, if fails from "stable" location

--- scripts/03a_read_spatial_habitat_data_from_gdb.R
+++ scripts/03a_read_spatial_habitat_data_from_gdb.R
@@ -1,54 +1,18 @@
-# Read geodatabase from network read a preprocessed file
-
-
-
-#read.network.geodatabase <- function(net.dir = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Marine_Evidence_Geodatabase.gdb",
-#                                     gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
-#        require(rgdal)
-#        gdb.n <- try(readOGR(dsn = net.dir, layer = gis.layer))
-#        
-#}
-
-#read.preprocessed.geodatabase <- function(prep.gdb.dir = "F:/projects/biotope-sensitivity/copy_data/Marine_Evidence_Geodatabase.gdb",
-#                                          gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
-#        require(rgdal)
-#        gdb.n <- readOGR(dsn = net.dir, layer = gis.layer)
-#        
-#}
-
-
-
-#r <- NULL
-#attempt <- 1
-#while( is.null(r) && attempt <= 3 ) {
-#        attempt <- attempt + 1
+# Read geodatabase from network, it it fails read a preprocessed file (a back-up copy that should not be changed unless certain that it is working)
+#status: the current network file specified is the full file - this will have to be changed to a directory where the latest preprocessed file is saved.
 read.network.geodatabase <- function(net.dir = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Marine_Evidence_Geodatabase.gdb",
                                             gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
         require(rgdal)
         gdb <- try(readOGR(dsn = net.dir, layer = gis.layer))
         if("try-error" %in% class(gdb)) {
-                #cat(“Caught an error during to read the network file, trying read a back up copy.\n”)
-                prep.gdb.dir <- "D:/projects/fishing_displacement/2_subprojects_and_data/2_GIS_DATA/Marine habitat/Marine_Evidence_Geodatabase.gdb"
+                cat("Caught an error during to read the network file, trying read a back up copy.\n")
+                prep.gdb.dir <- "D:/projects/fishing_displacement/2_subprojects_and_data/2_GIS_DATA/Marine habitat/Input_Polys_WGS84_Internal_BGR_inside_12nm.gpkg"
                         #"F:/projects/biotope-sensitivity/copy_data/Marine_Evidence_Geodatabase.gdb"
-                gis.layer <- "Input_BSH_Polys_WGS84_Internal"
+                gis.layer <- "habitat_inside_12nm"
                 gdb <- readOGR(dsn = prep.gdb.dir, layer = gis.layer)
                 
                 }
         gdb
 }
 
-read.network.geodatabase()
-
-readOGR(dsn = "F:/projects/biotope-sensitivity/copy_data/Marine_Evidence_Geodatabase.gdb", layer = "Input_BSH_Polys_WGS84_Internal")
-
-#read.file <- function (file.name) {
-#        require(data.table)
-#        file <- try(fread(file.name))
- #       if (class(file) == “try-error”) {
- #               cat(“Caught an error during fread, trying read.table.\n”)
- #               file <- as.data.table(read.table(file.name, sep = ” “, quote = “”))
- #       }
- #       file
-#}
-
-        
+gpkg <- read.network.geodatabase() # calls the function which will read the habitat file.
