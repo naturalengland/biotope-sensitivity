50250de78a1ec094ac32f0a814f4c46cb20601c8
Philip Haupt <philip.haupt@gmail.com>
2018-11-26 11:54:47 +0000

    #rename 08b to main file 08; remove commented out code

--- scripts/08_sbgr_BAP.R
+++ scripts/08_sbgr_BAP.R
@@ -1,39 +1,28 @@
-# join the pressure to a sbgr-consolidated data set
+# join the pressure to a sbgr-consolidated data set: rewritten to join within activity rather than activity within sbgr
 library(plyr)
 library(dplyr)
 library(magrittr)
 
-
-
-sbgr.bap <- sbgr.matched.btpt.w.rpl %>% 
-        plyr::llply(function(x) { 
+xap.ls <- act.press.list.2 %>% 
+        plyr::llply(function(y){
                 
+                tidy.p <- y 
                 
-                
-                #x.rn <- x$eunis.code.gis # this is the EUNIs codes of the GIS files, which will become the row names for each data frame
-                
-                #simplified data set
-                x.df <- #x %>% dplyr::select(eunis.code.gis, 1:14, 19:354) %>%
-                        x %>% dplyr::select(eunis.code.gis, sbgr, 1:14, 19:354) %>%
-                        tidyr::gather("A5.22":"A5.7211",key = "eunis.assessed",value = "eunis.gis") %>%
-                        filter(eunis.gis != "<NA>") %>%
-                        select(sbgr, eunis.code.gis, eunis.match.assessed = eunis.assessed) %>%
-                        arrange(sbgr, eunis.code.gis,eunis.match.assessed)
-                
-                xap.ls <- act.press.list.2 %>% 
-                        plyr::llply(function(y){
-                                tidy.p <- y %>% 
-                                        rename(eunis.match.assessed = EUNISCode)
-                                xp.df <- right_join(x.df, tidy.p, by = "eunis.match.assessed")
+                                sbgr.bap <- sbgr.matched.btpt.w.rpl %>% 
+                        plyr::llply(function(x) { 
+                                
+                                x.df <- x %>% dplyr::select(eunis.code.gis, sbgr, 1:14, 19:354) %>%
+                                        tidyr::gather("A5.22":"A5.7211",key = "eunis.assessed",value = "eunis.gis") %>%
+                                        filter(eunis.gis != "<NA>" | eunis.gis != "NA") %>%
+                                        select(sbgr, eunis.code.gis, eunis.match.assessed = eunis.assessed) %>%
+                                        arrange(sbgr, eunis.code.gis,eunis.match.assessed)
+                                
+                                
+                                xp.df <- right_join(x.df, tidy.p, by = c("eunis.match.assessed" = "EUNISCode"))
                                 
                         })
                 
-
-                return(xap.ls)
+                
+                return(sbgr.bap)
         }, .progress = "text")
-saveRDS(sbgr.bap,"./output/sbgr_bap.rds")
-        
-
-
-
-
+saveRDS(xap.ls,"./output/xap_ls.rds")
