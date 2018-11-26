b541fc66e8dd552631087130a708a311bf25537f
Philip Haupt <philip.haupt@gmail.com>
2018-11-26 17:24:46 +0000

    add default function values

--- functions/match_eunis_to_biotope_fn.R
+++ functions/match_eunis_to_biotope_fn.R
@@ -2,8 +2,9 @@
 #Match all lower level EUNIs habitats to eunis level 6 that share the same EUNIS composition codes
 # to work on: not able to pass results on into a global variable...
 
+## Check and create a directory for output
 
-match_eunis_to_biotopes_fn <- function(x, y, high.lvl) {
+match_eunis_to_biotopes_fn <- function(x = x, y = bgr.dfs.lst, high.lvl = mx.lvl) {
         # x is a character vector, containing the eunis level for which there have been sensitivity assessments (coming from PD_AoO database)
         # y is a list of dataframes, containing any eunis level lower than eunis 6 for which we want to obtain eunis level 6 data (coming from the spatial database)
         # high.lvl is a integer value, specifying the maximum EUNIS level at which results are dealt with
@@ -22,7 +23,7 @@ match_eunis_to_biotopes_fn <- function(x, y, high.lvl) {
                 
                 #split y[[h]] by eunis "level" to allo associating biotopes with heirarchical levels of eunis codes (to allow using highest level eunis codes in the end)
                 y.dfs.lst <- split(y[[h]], f = y[[h]]$level) # [[h]] becuase splitting an already split list of dataframes, [[h]] refers to iterative dataframes in the list of dataframes held in y
-    
+                
                 result.sbgr <- vector("list", length(y))
                 # within each subBGR number of dataframes held within y.dfs.lst, needed to inform the z matrix subsets: the bit of spatial data that we want to work with!
                 for (k in seq_along(y.dfs.lst)) {
@@ -32,13 +33,13 @@ match_eunis_to_biotopes_fn <- function(x, y, high.lvl) {
                         sBGR <- unique(y.dfs.lst[[k]]$bgr_subreg_id) # the name of the biotope
                         lvl.col <- which( colnames(y.dfs.lst[[k]])=="level" ) # determines which column is called "level" to add to saved results name
                         low.lvl <- unique(y.dfs.lst[[k]][[lvl.col]]) # generates a single value of EUNIS level of spatial data being assessed (one level is being asssessed at a time)
-      
+                        
                         #determine the number of character to filter the results by
                         nchar.substr <- ifelse(low.lvl < "3",as.integer(low.lvl),as.integer(low.lvl)+1) # becuase EUNIs codes have a "." after teh 2nd char, we have to add a character to eunis levels more than three
-      
+                        
                         # generate an empty temporary data frame to store the matched x and z results-----------------------------------
                         result.tbl <- data.frame(matrix(ncol = length(x),nrow = length(z)))
-      
+                        
                         #Why For loops: to make sure that each element of y is compared to each element of x two (nested) for loops are used to cycle through each element in y, and each element in x_____________________________
                         for (i in seq_along(z)) {# repeats the following commands for each element in y 
                                 a <- matrix(ncol = length(x), nrow = length(z)) # generate a matrix with the same number of columns as x, and same number of rows as y in which TRUE or FALSE is stored
@@ -56,21 +57,21 @@ match_eunis_to_biotopes_fn <- function(x, y, high.lvl) {
                                 names(result.tbl) <- as.character(x) #assign column names based on biotope names
                                 row.names(result.tbl) <- as.character(z) # assign column names
                         }
-    
-                                                # add columns to results, and then save individual tables
+                        
+                        # add columns to results, and then save individual tables
                         result.tbl$sbgr <- as.character(sBGR)
                         result.tbl$h.lvl <- as.character(high.lvl)
                         result.tbl$l.lvl <- as.character(low.lvl)
                         result.tbl$eunis.code.gis <- row.names(result.tbl)
                         row.names(result.tbl) <- c()
                         
+                        # WRITE RESULTS:
                         #write to file for later use, and verification
-                        write.csv(result.tbl,paste0("./output/sbgr_biotp_eunis_match/subBGR_",sBGR,"_match_biotope_eunis_high_",high.lvl,"_eunis_mapped_",low.lvl,".csv")) # stores the result as a csv
+                        write.csv(result.tbl,paste0("./subBGR_",sBGR,"_match_biotope_eunis_high_",high.lvl,"_eunis_mapped_",low.lvl,".csv")) # stores the result as a csv
                         #saveRDS(result.tbl, paste0("./output/sbgr_biotp_eunis_match/subBGR_",sBGR,"_match_biotope_eunis_high_",high.lvl,"_eunis_mapped_",low.lvl,".rds"))
                         
                         #result.tbl # on screen viewing
                         print(paste("Finished processing and saved level ",high.lvl, " for subBiographic region: ",sBGR, " at: ", Sys.time() ))
-                        
                         #write results to big table as R obj
                         # "g" comes from for loop from where it is being called
                         #level.result.tbl[[g]] <- rbind(level.result.tbl[[g]], result.tbl)
@@ -87,3 +88,5 @@ match_eunis_to_biotopes_fn <- function(x, y, high.lvl) {
         assign("out", "result.btp.lvl",envir = globalenv())
         #level.result.tbl[[g]] <- result.btp.lvl
 }
+
+
