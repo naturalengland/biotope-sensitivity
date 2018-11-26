2e789e0d4d1ed2909b9d38bd3be387048ba49569
Philip Haupt <philip.haupt@gmail.com>
2018-11-26 10:30:58 +0000

    #update GIS read and clean functions; needs backup locality to be made "general"

--- scripts/04_clean_and_prep_gis_data.R
+++ scripts/04_clean_and_prep_gis_data.R
@@ -1,22 +1,35 @@
-#Clean geodata file
+#Clean geodata file; done from attribute table - i.e. remove the geomotry to make the file small and managable to work with.
 #to do: functionalise: make gis.hab.bgr.dat a changable variable:#specify unique id and other variables
-
 
 library(tidyverse)
 
-#define variables
-gis.attr <- gpkg@data # input data
-names(gis.attr)
+#function that reads file from specified locality, or defaults to a back-up locality, # needs working up
+load.gis.attrib <- function(attr = hab.map@data){ #hab.map is the habitat map that was read in 03_read_spatial_habitat_data
+        gis.attr <- try(attr) # input data from habitat map - if error read the hab map attributes from a back file
+        if("try-error" %in% class(gis.attr)) {
+                cat("The GIS habitat map is not read in, or there is an error, trying read a back up copy of attribute file.\n")
+                backup.attribute.tbl <- "C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/4_R/sensitivities_per_pressure/gisattr.rds"
+                gis.attr <- read_rds(path = backup.attribute.tbl)
+                
+        }
+        cat("Make sure that the gis attribute and habitat types have the same number of observations.\n")
+        gis.attr
+}
+
+gis.attr <- load.gis.attrib()
 
 #clean data
 gis.hab.bgr.dat <- function(dat = gis.attr){
-        
+        require(dplyr)
+        d <- rownames(dat)
+        if(is.null(dat$OBJECTID))
+                dat$OBJECTID <- d
         #clean HAB_TYPE column from multiple entries
         dat$HAB_TYPE <- gsub(" or ", "/", dat$HAB_TYPE) # replace ; with / to make consistent
         dat$HAB_TYPE <- gsub(";", "/", dat$HAB_TYPE) # replace ; with / to make consistent
         dat$HAB_TYPE <- gsub("(8)", "", dat$HAB_TYPE) # remove (8) to make consistent
         dat$HAB_TYPE <- gsub(" #", "", dat$HAB_TYPE) # remove (8) to make consistent
-
+        
         # Separate HAB_TYPE into multiple columns where "/" appears to allow for the next step
         hab.types <- dat %>%
                 select(OBJECTID, HAB_TYPE, bgr_subreg_id = SubReg_id) %>%
@@ -25,8 +38,9 @@ gis.hab.bgr.dat <- function(dat = gis.attr){
         hab.types <- purrr::map_df(hab.types, function(x) trimws(x, which = c("both")))
         str(hab.types) # changed integer top char for all!
         hab.types$OBJECTID <- as.integer(hab.types$OBJECTID)
-
-
+        
+        
+        
         #check if there are values in all columns, and drop columns with no values
         hab.types %>%
                 distinct(hab.4)
@@ -37,7 +51,7 @@ gis.hab.bgr.dat <- function(dat = gis.attr){
         hab.types %>%
                 distinct(hab.3)
         #has multiple values - keep
-
+        hab.types
 }
 
-gis.hab.bgr.dat()
+hab.types <- gis.hab.bgr.dat(gis.attr)
