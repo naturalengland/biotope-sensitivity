c28cc6db67d9f7c77ea8975ad5adc893ad06de9a
Philip Haupt <philip.haupt@gmail.com>
2018-11-26 10:23:41 +0000

    # add the option to choose a file if it fails to find teh gdb in the default location; code added to read a test file for fast running tests

--- scripts/03a_read_spatial_habitat_data_from_gdb.R
+++ scripts/03a_read_spatial_habitat_data_from_gdb.R
@@ -1,23 +1,36 @@
+####ONL RUN TOP BIT FOR TEST SMALL TEST FILE
+
+#Libraries
+library(rgdal)
+
+
 # Read geodatabase from network, it it fails read a preprocessed file (a back-up copy that should not be changed unless certain that it is working)
 #status: the current network file specified is the full file - this will have to be changed to a directory where the latest preprocessed file is saved.
-read.network.geodatabase <- function(net.dir = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Marine_Evidence_Geodatabase.gdb",
-                                            gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
+read.network.geodatabase <- function(net.dir = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Marine_Evidence_Geodatabase\\Marine_Evidence_Base_Internal.gdb",#\\SAMVW3-GIREP02\NEWorkingData\GIS_Working_Data\Marine\Marine_Evidence_Geodatabase\
+                                     gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
         require(rgdal)
         gdb <- try(readOGR(dsn = net.dir, layer = gis.layer))
         if("try-error" %in% class(gdb)) {
                 cat("Caught an error during to read the network file, trying read a back up copy.\n")
                 prep.gdb.dir <- "D:\\projects\\fishing_displacement\\2_subprojects_and_data\\2_GIS_DATA\\Marine habitat\\sbgr_input_poly_wgs84_internal_bgr_inside_12nm.gpkg"
-                #D:\projects\fishing_displacement\2_subprojects_and_data\2_GIS_DATA\Marine habitat        
-                #"D:/projects/fishing_displacement/2_subprojects_and_data/2_GIS_DATA/Marine habitat/Input_Polys_WGS84_Internal_BGR_inside_12nm.gpkg"
-                        #"F:/projects/biotope-sensitivity/copy_data/Marine_Evidence_Geodatabase.gdb"
                 gis.layer <- "sbgr_input_poly_wgs84_internal_bgr_inside_12nm"
-                gdb <- readOGR(dsn = prep.gdb.dir, layer = gis.layer)
-                
+                gdb <- try(readOGR(dsn = prep.gdb.dir, layer = gis.layer))
+                if("try-error" %in% class(gdb)) {
+                        cat("Could not load back-up file, please specify the locality of the geodatabase./n")
+                        gdb.path <- as.character(file.choose())
+                        cat("Now specify the layer, e.g. Input_BSH_Polys_WGS84_Internal")
+                        layer. <- basename(as.character(file.choose()))
+                        gdb <- readOGR(dsn = gdb.path, layer = layer.)
                 }
+                gdb
+        }
         gdb
 }
 
-gpkg <- read.network.geodatabase() # calls the function which will read the habitat file.
+hab.map <- read.network.geodatabase() # calls the function which will read the habitat file.
 
 
-#gdb <- (readOGR(dsn = net.dir, layer = gis.layer))
+#smaller test map
+#hab.map <- readOGR(dsn = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Phil_Haupt\\Fishing_effort_displacement\\2_Data\\1_QGIS\\test_habmap_for_R.gpkg", layer = "test_habmap_for_R")
+#hab.map <- readOGR(dsn = "C:\\Users\\M996613\\Phil\\PROJECTS\\Fishing_effort_displacement\\2_subprojects_and_data\\2_GIS_DATA\\tmp_test\\test_habmap_for_R.gpkg", layer = "test_habmap_for_R")
+#hab.map$SubReg_id <- "3a"
