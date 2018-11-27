7f239bc78b661c14e706f63f096872cbd6424cbc
Philip Haupt <philip.haupt@gmail.com>
2018-11-27 13:38:16 +0000

    commented out script in progress to make function work from 1 - 13

--- scripts/01_connect_to_access_db.R
+++ scripts/01_connect_to_access_db.R
@@ -22,7 +22,7 @@ library(tidyverse) # to use piping and other data wrangling functions.
 
 
 #TEST RUN: ONLY THIS LINE
-qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
+#qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
 
 
 
@@ -39,15 +39,9 @@ qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort
 db.path <- "D:/projects/fishing_displacement/2_subprojects_and_data/5_internal_data_copies/database/PD_AoO.accdb"
 
 drv.path <- "Microsoft Access Driver (*.mdb, *.accdb)" #"SQL Server"#or #
-srv.host <- "Null"#e.g. "mysqlhost"
+#srv.host <- "Null"#e.g. "mysqlhost"
 
-try(read.access.db(db.path,drv.path))
-if("try-error" %in% class(read.access.db(db.path,drv.path))){
-        qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
-}
-
-
-read.access.db <- function(db.p = db.path, drv.p = drv.path, srv.host = NULL){
+read.access.db <- function(db.p = db.path, drv.p = drv.path){
         
         #MySQL: 
         #dbhandle <- odbcDriverConnect('driver={SQL Server};server=mysqlhost;database=mydbname;trusted_connection=true')
@@ -68,11 +62,11 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path, srv.host = NULL){
         
         ## Inspect data
         ## List of Tables
-        subset(sqlTables(conn), TABLE_TYPE == "TABLE") %>%
-                arrange(TABLE_NAME)
+        #subset(sqlTables(conn), TABLE_TYPE == "TABLE") %>%
+        #        arrange(TABLE_NAME)
         ## List of VIEW queries
-        subset(sqlTables(conn), TABLE_TYPE == "VIEW")%>%
-                arrange(TABLE_NAME)
+        #subset(sqlTables(conn), TABLE_TYPE == "VIEW")%>%
+        #        arrange(TABLE_NAME)
         
         
         # Get data
@@ -80,36 +74,36 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path, srv.host = NULL){
         ## could also just read in query 3
         
         #1: Step 1 if queries are in Access this step is NOT required, but can be run to VIEW the data; if queries not housed in PD_AoO database this step is neccessary
-        qryEUNISFeatAct <- sqlQuery( conn , paste("SELECT tblEUNISFeature.EUNISCode, tblFeatureActivity.FeatSubHabCode, tblFeatureActivity.FARelevant, tblActivityLUT.OperationCode, tblFeatureActivity.ActivityCode, tblActivityLUT.ActivityName 
-                                                  FROM tblActivityLUT INNER JOIN (tblFeatureActivity INNER JOIN tblEUNISFeature ON tblFeatureActivity.FeatSubHabCode = tblEUNISFeature.FeatSubHabCode) ON tblActivityLUT.ActivityCode = tblFeatureActivity.ActivityCode 
-                                                  WHERE (((tblFeatureActivity.FARelevant)='Yes') AND ((tblActivityLUT.OperationCode)='11'));")) #by changing the very last value '11' to any other , it will change the operation considered. by remvoing it, all operations will be included.
+        #qryEUNISFeatAct <- sqlQuery( conn , paste("SELECT tblEUNISFeature.EUNISCode, tblFeatureActivity.FeatSubHabCode, tblFeatureActivity.FARelevant, tblActivityLUT.OperationCode, tblFeatureActivity.ActivityCode, tblActivityLUT.ActivityName 
+        #                                          FROM tblActivityLUT INNER JOIN (tblFeatureActivity INNER JOIN tblEUNISFeature ON tblFeatureActivity.FeatSubHabCode = tblEUNISFeature.FeatSubHabCode) ON tblActivityLUT.ActivityCode = tblFeatureActivity.ActivityCode 
+        #                                          WHERE (((tblFeatureActivity.FARelevant)='Yes') AND ((tblActivityLUT.OperationCode)='11'));")) #by changing the very last value '11' to any other , it will change the operation considered. by remvoing it, all operations will be included.
         
         # step 2a if queries are in Access this step is NOT required, but can be run to VIEW the data; if queries not housed in PD_AoO database, skip to next step
-        qryEUNIS_grp_act <- sqlQuery( conn , paste("SELECT qryEUNISFeatAct.EUNISCode, qryEUNISFeatAct.ActivityCode, qryEUNISFeatAct.ActivityName FROM qryEUNISFeatAct
-                                                   GROUP BY qryEUNISFeatAct.EUNISCode, qryEUNISFeatAct.ActivityCode, qryEUNISFeatAct.ActivityName;"))
+        #qryEUNIS_grp_act <- sqlQuery( conn , paste("SELECT qryEUNISFeatAct.EUNISCode, qryEUNISFeatAct.ActivityCode, qryEUNISFeatAct.ActivityName FROM qryEUNISFeatAct
+        #                                           GROUP BY qryEUNISFeatAct.EUNISCode, qryEUNISFeatAct.ActivityCode, qryEUNISFeatAct.ActivityName;"))
         
         #this should be the same as step 2a using dplyr
-        qryEUNIS_grp_act <- qryEUNISFeatAct %>% select(EUNISCode, 
-                                                       ActivityCode,
-                                                       ActivityName) %>%
-                group_by(EUNISCode,ActivityCode, ActivityName) %>%
-                distinct()
+        #qryEUNIS_grp_act <- qryEUNISFeatAct %>% select(EUNISCode, 
+        #                                               ActivityCode,
+        #                                               ActivityName) %>%
+        #        group_by(EUNISCode,ActivityCode, ActivityName) %>%
+        #        distinct()
         #----------------------------------------------------------------------------------------
         #read in additional tables needed fpor query (using dplyr with dbplyr backend) # not tested
-        copy_to(conn, tblEUNISLUT)
-        tblEUNISLUT <- tbl(conn, "tblEUNISLUT")
+        #copy_to(conn, tblEUNISLUT)
+        #tblEUNISLUT <- tbl(conn, "tblEUNISLUT")
         
-        copy_to(conn, tblEUNISPressure)
-        tblEUNISPressure <- tbl(conn, "tblEUNISPressure")
+        #copy_to(conn, tblEUNISPressure)
+        #tblEUNISPressure <- tbl(conn, "tblEUNISPressure")
         
-        copy_to(conn, tblActivityPressure)
-        tblActivityPressure <- tbl(conn, "tblActivityPressure")
+        #copy_to(conn, tblActivityPressure)
+        #tblActivityPressure <- tbl(conn, "tblActivityPressure")
         
-        copy_to(conn, tblPressureLUT)
-        tblPressureLUT <- tbl(conn, "tblPressureLUT")
+        #copy_to(conn, tblPressureLUT)
+        #tblPressureLUT <- tbl(conn, "tblPressureLUT")
         
-        copy_to(conn, tblSensitivityLUT)
-        tbl(conn, "tblSensitivityLUT")
+        #copy_to(conn, tblSensitivityLUT)
+        #tbl(conn, "tblSensitivityLUT")
         
         #qryEUNIs_grp_act
         #use this to reconstruct the below:
@@ -128,19 +122,24 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path, srv.host = NULL){
         #qryEUNIS_ActPressSens <- inner_join()
         
         #this query lists the sensitivities of each eunis code 
-        qrySeaRegion_EUNIS_Sens <- sqlQuery(conn, paste("SELECT tblEUNISFeature.EUNISCode, tblEUNISFeature.FeatSubHabCode, qrySeaRegion_relevant_biotopes.BGRCode, tblEUNISPressure.PressureCode, tblEUNISPressure.Sensitivity, tblSensitivityLUT.ActSensRank, tblSensitivityLUT.SensPriority 
-                                                        FROM tblSensitivityLUT INNER JOIN ((qrySeaRegion_relevant_biotopes INNER JOIN tblEUNISFeature ON qrySeaRegion_relevant_biotopes.EUNISCode = tblEUNISFeature.EUNISCode) 
-                                                        INNER JOIN tblEUNISPressure ON tblEUNISFeature.EUNISCode = tblEUNISPressure.EUNISCode) ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity WHERE (((tblSensitivityLUT.SensPriority)<'7'));"))
+        #qrySeaRegion_EUNIS_Sens <- sqlQuery(conn, paste("SELECT tblEUNISFeature.EUNISCode, tblEUNISFeature.FeatSubHabCode, qrySeaRegion_relevant_biotopes.BGRCode, tblEUNISPressure.PressureCode, tblEUNISPressure.Sensitivity, tblSensitivityLUT.ActSensRank, tblSensitivityLUT.SensPriority 
+        #                                                FROM tblSensitivityLUT INNER JOIN ((qrySeaRegion_relevant_biotopes INNER JOIN tblEUNISFeature ON qrySeaRegion_relevant_biotopes.EUNISCode = tblEUNISFeature.EUNISCode) 
+        #                                                INNER JOIN tblEUNISPressure ON tblEUNISFeature.EUNISCode = tblEUNISPressure.EUNISCode) ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity WHERE (((tblSensitivityLUT.SensPriority)<'7'));"))
         #rm(qrySeaRegion_EUNIS_Sens)
         
         #This query lists the EUNIs codes of the biotopes which are known to occur within each of the sub-biogeographic regions (sbgr).
-        qrySeaRegion_relevant_biotopes <- sqlQuery(conn, paste("SELECT tblEUNISBiogeoRegion.EUNISCode, tblEUNISBiogeoRegion.BGRCode, tblEUNISBiogeoRegion.RelevantToRegion, qrySeaRegionLUT_relevant.RegionalSea
-                                                               FROM tblEUNISBiogeoRegion INNER JOIN qrySeaRegionLUT_relevant ON tblEUNISBiogeoRegion.BGRCode = qrySeaRegionLUT_relevant.BGRCode
-                                                               WHERE (((tblEUNISBiogeoRegion.RelevantToRegion)='Yes'));"))
+        #qrySeaRegion_relevant_biotopes <- sqlQuery(conn, paste("SELECT tblEUNISBiogeoRegion.EUNISCode, tblEUNISBiogeoRegion.BGRCode, tblEUNISBiogeoRegion.RelevantToRegion, qrySeaRegionLUT_relevant.RegionalSea
+        #                                                       FROM tblEUNISBiogeoRegion INNER JOIN qrySeaRegionLUT_relevant ON tblEUNISBiogeoRegion.BGRCode = qrySeaRegionLUT_relevant.BGRCode
+        #                                                      WHERE (((tblEUNISBiogeoRegion.RelevantToRegion)='Yes'));"))
         
         ##house kepping: close the connection: S3 method for class 'RODBC'
-        close(con = conn)
+        
 }
 
-read.access.db()
+qryEUNIS_ActPressSens <- try(read.access.db(db.path,drv.path)) 
+if("try-error" %in% class(read.access.db(db.path,drv.path))) {
+        qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
+}
+
 rm(db.path,drv.path,srv.host)
+#close(con = conn)
