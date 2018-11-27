a00d4307e6c4027d3f90897b4fbec82acd289889
Philip Haupt <philip.haupt@gmail.com>
2018-11-27 13:39:21 +0000

    #force a new primary key as objectid is not unique!

--- scripts/04_clean_and_prep_gis_data.R
+++ scripts/04_clean_and_prep_gis_data.R
@@ -22,8 +22,8 @@ gis.attr <- load.gis.attrib()
 gis.hab.bgr.dat <- function(dat = gis.attr){
         require(dplyr)
         d <- rownames(dat)
-        if(is.null(dat$OBJECTID))
-                dat$OBJECTID <- d
+        #if(is.null(dat$pkey)) # there are duplcaited pkeys - as this was a column in the GIS, which got split when intersected with sbgr...think about updating the pkey in the GIS
+                dat$pkey <- d
         #clean HAB_TYPE column from multiple entries
         dat$HAB_TYPE <- gsub(" or ", "/", dat$HAB_TYPE) # replace ; with / to make consistent
         dat$HAB_TYPE <- gsub(";", "/", dat$HAB_TYPE) # replace ; with / to make consistent
@@ -32,12 +32,12 @@ gis.hab.bgr.dat <- function(dat = gis.attr){
         
         # Separate HAB_TYPE into multiple columns where "/" appears to allow for the next step
         hab.types <- dat %>%
-                select(OBJECTID, HAB_TYPE, bgr_subreg_id = SubReg_id) %>%
+                select(pkey, HAB_TYPE, bgr_subreg_id = SubReg_id) %>%
                 tidyr::separate(HAB_TYPE, into = c("hab.1", "hab.2", "hab.3", "hab.4"), sep = "/", remove = F)
         # Remove any leading or trailing white spaces which could cause problems when matching the eunis columns between gis and database.
         hab.types <- purrr::map_df(hab.types, function(x) trimws(x, which = c("both")))
         str(hab.types) # changed integer top char for all!
-        hab.types$OBJECTID <- as.integer(hab.types$OBJECTID)
+        hab.types$pkey <- as.integer(hab.types$pkey)
         
         
         
