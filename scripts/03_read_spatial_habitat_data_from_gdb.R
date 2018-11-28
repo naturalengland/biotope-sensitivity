77b5207f68e4f63a4fae4b0aa380da1f749d0cc5
Philip Haupt <philip.haupt@gmail.com>
2018-11-28 09:44:42 +0000

    remove script and point to function to read GIS

--- scripts/03_read_spatial_habitat_data_from_gdb.R
+++ scripts/03_read_spatial_habitat_data_from_gdb.R
@@ -6,28 +6,10 @@ library(rgdal)
 
 # Read geodatabase from network, it it fails read a preprocessed file (a back-up copy that should not be changed unless certain that it is working)
 #status: the current network file specified is the full file - this will have to be changed to a directory where the latest preprocessed file is saved.
-read.network.geodatabase <- function(net.dir = "\\\\SAMVW3-GIREP02\\NEWorkingData\\GIS_Working_Data\\Marine\\Marine_Evidence_Geodatabase\\Marine_Evidence_Base_Internal.gdb",#\\SAMVW3-GIREP02\NEWorkingData\GIS_Working_Data\Marine\Marine_Evidence_Geodatabase\
-                                     gis.layer = "Input_BSH_Polys_WGS84_Internal" ){
-        require(rgdal)
-        gdb <- try(readOGR(dsn = net.dir, layer = gis.layer))
-        if("try-error" %in% class(gdb)) {
-                cat("Caught an error during to read the network file, trying read a back up copy.\n")
-                prep.gdb.dir <- "D:\\projects\\fishing_displacement\\2_subprojects_and_data\\2_GIS_DATA\\Marine habitat\\sbgr_input_poly_wgs84_internal_bgr_inside_12nm.gpkg"
-                gis.layer <- "sbgr_input_poly_wgs84_internal_bgr_inside_12nm"
-                gdb <- try(readOGR(dsn = prep.gdb.dir, layer = gis.layer))
-                if("try-error" %in% class(gdb)) {
-                        cat("Could not load back-up file, please specify the locality of the geodatabase./n")
-                        gdb.path <- as.character(file.choose())
-                        cat("Now specify the layer, e.g. Input_BSH_Polys_WGS84_Internal")
-                        layer. <- basename(as.character(file.choose()))
-                        gdb <- readOGR(dsn = gdb.path, layer = layer.)
-                }
-                gdb
-        }
-        gdb
-}
+source(file = "./functions/read_gis_hab_lr_fn.R")
 
-hab.map <- read.network.geodatabase() # calls the function which will read the habitat file.
+# calls the function which will read the habitat file.
+hab.map <- read.network.geodatabase() 
 
 ####ONLY RUN BELOW FOR TEST SMALL TEST FILE
 #smaller test map
