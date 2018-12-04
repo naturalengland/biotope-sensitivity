b63b2d80813d4e14852cc4d2774126cefffd9440
Philip Haupt <philip.haupt@gmail.com>
2018-12-04 07:53:11 +0000

    add "dplyr::" before cammand as plyr also has select and group functions which get confused

--- functions/gis_sbgr_hab_max_sens_fn.R
+++ functions/gis_sbgr_hab_max_sens_fn.R
@@ -3,24 +3,31 @@ act.sbgr.bps.gis <- sbgr.BAP.max.sens %>%
         llply(function(x){ # split the list into its dataframes, and provide a list at the end (containing dataframes) (which are eventaully piped onto bind_cols into a single dataframe /preferred over ldply as this would cause row bind, not column bind.
                 require(stringr)
                 require(dplyr)
+                require(tidyr)
                 
                 #Test code - remove once the script is working:#test code only - to test a single matrix at a time
                 #x <- sbgr.BAP.max.sens[[1]] 
                 
-                #join the GIS file to the dataframes coming from sbgr.BAP.max.sens to obbtain the ID number for the individual polygons into the data set.
-                sbgr.hab.gis <- left_join(hab.types, x, by = c("bgr_subreg_id" = "sbgr", "hab.1" = "eunis.code.gis")) %>% #  composite join, e.g.: left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
-                        select(pkey, sbgr = bgr_subreg_id, eunis.code.gis = hab.1, eunis.match.assessed, ActivityCode, PressureCode, max.sens) %>%
-                        spread(key = PressureCode, value = max.sens)
+                #join the GIS file to the dataframes coming from sbgr.BAP.max.sens to obtain the ID number for the individual polygons into the data set.
+                sbgr.hab.gis <- dplyr::left_join(hab.types, x, by = c("bgr_subreg_id" = "sbgr", "hab.1" = "eunis.code.gis")) %>% #  composite join, e.g.: left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
+                        dplyr::select(pkey, sbgr = bgr_subreg_id, eunis.code.gis = hab.1, eunis.match.assessed, ActivityCode, PressureCode, max.sens)
+                        #tidyr::spread(key = PressureCode, value = max.sens)
                 
                 #drop columns with NA for name (these may have arised in datasets where NA occured in the PressureCode column which were assigned NA if they were not present)
-                sbgr.hab.gis <- sbgr.hab.gis %>% select(-("<NA>"))
+                #sbgr.hab.gis <- sbgr.hab.gis %>% select(-("<NA>"))
                 
                 #generate a single maximum value per column (there are currently multiple sensitivity scores associated with each)
+                #sbgr.hab.gis.2  <-  sbgr.hab.gis %>%
+                #        dplyr::select(-(2:5)) %>% # remove not immediately relevant columns
+                #        dplyr::group_by(eval(group.by)) %>% # group the results by a unique identifier for polygon: "pkey"
+                #        dplyr::summarise_all(max) %>% # summarise (obtain the maximum) ALL the remaining columns (according to the grouping); this was key to ensuring that dynamic names operate; if columns 2:5 were needed that could be bound after the fact, as these variables cannot be summarised as they are not numerical
+                #        dplyr::select(-(`eval(group.by)`)) #remove the surpuflous variable created from using parse/eval functions.
+                #carry on: 
                 sbgr.hab.gis.2  <-  sbgr.hab.gis %>%
-                        select(-(2:5)) %>% # remove not immediately relevant columns
-                        group_by(eval(group.by)) %>% # group the results by a unique identifier for polygon: "pkey"
-                        summarise_all(max) %>% # summarise (obtain the maximum) ALL the remaining columns (according to the grouping); this was key to ensuring that dynamic names operate; if columns 2:5 were needed that could be bound after the fact, as these variables cannot be summarised as they are not numerical
-                        select(-(`eval(group.by)`)) #remove the surpuflous variable created from using parse/eval functions.
+                        dplyr::ungroup() %>%
+                        dplyr::group_by(ActivityCode, PressureCode, pkey) %>%
+                        dplyr::summarise(max.sens.consolidate = max(max.sens, na.rm=TRUE)) %>%
+                        tidyr::spread(key = PressureCode, value = max.sens.consolidate)
                 
                 
                 # Make unique names for each dataframe so that they can be put into a single data frame at the end:
@@ -36,4 +43,5 @@ act.sbgr.bps.gis <- sbgr.BAP.max.sens %>%
                 
                 
         }, .progress = "text") %>% #provides an indication of progress in executing the code. 
-        bind_cols() # binds the results (columns) from each list, keeping the original identifier as the main id.
+        bind_cols() %>% # binds the results (columns) from each list, keeping the original identifier as the main id.
+        dplyr::select(-one_of("pkey1", "pkey2", "pkey3", "pkey4", "pkey5", "pkey6", "pkey7","pkey8"))
