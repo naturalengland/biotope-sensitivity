f949dda6d95ed3e8b173be75a38f9a17a3e6c59c
Philip Haupt <philip.haupt@gmail.com>
2018-12-13 11:39:54 +0000

    add drop column functionality for <NA> columns,  add heatmap value into table

--- functions/gis_sbgr_hab_max_sens_fn.R
+++ functions/gis_sbgr_hab_max_sens_fn.R
@@ -5,31 +5,18 @@ act.sbgr.bps.gis <- sbgr.BAP.max.sens %>%
                 require(dplyr)
                 require(tidyr)
                 
-                #Test code - remove once the script is working:#test code only - to test a single matrix at a time
-                #x <- sbgr.BAP.max.sens[[1]] 
-                
                 #join the GIS file to the dataframes coming from sbgr.BAP.max.sens to obtain the ID number for the individual polygons into the data set.
                 sbgr.hab.gis <- dplyr::left_join(hab.types, x, by = c("bgr_subreg_id" = "sbgr", "hab.1" = "eunis.code.gis")) %>% #  composite join, e.g.: left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
                         dplyr::select(pkey, sbgr = bgr_subreg_id, eunis.code.gis = hab.1, eunis.match.assessed, ActivityCode, PressureCode, max.sens)
-                        #tidyr::spread(key = PressureCode, value = max.sens)
+
                 
                 
                 #generate a single maximum value per column (there are currently multiple sensitivity scores associated with each)
-                #sbgr.hab.gis.2  <-  sbgr.hab.gis %>%
-                #        dplyr::select(-(2:5)) %>% # remove not immediately relevant columns
-                #        dplyr::group_by(eval(group.by)) %>% # group the results by a unique identifier for polygon: "pkey"
-                #        dplyr::summarise_all(max) %>% # summarise (obtain the maximum) ALL the remaining columns (according to the grouping); this was key to ensuring that dynamic names operate; if columns 2:5 were needed that could be bound after the fact, as these variables cannot be summarised as they are not numerical
-                #        dplyr::select(-(`eval(group.by)`)) #remove the surpuflous variable created from using parse/eval functions.
-                #carry on: 
                 sbgr.hab.gis.2  <-  sbgr.hab.gis %>%
                         dplyr::ungroup() %>%
                         dplyr::group_by(PressureCode, pkey) %>%
                         dplyr::summarise(max.sens.consolidate = max(max.sens, na.rm=TRUE)) %>%
                         tidyr::spread(key = PressureCode, value = max.sens.consolidate)
-                
-                ##REMOVE LIKE NA columns!!! TO DO!!!!
-                #drop columns with NA for name (these may have arised in datasets where NA occured in the PressureCode column which were assigned NA if they were not present)
-                try(sbgr.hab.gis <- sbgr.hab.gis %>% select(-("<NA>"))) # this raises an error message as says there is n column called <NA> - but we know there are...so needs sorting
                 
                 
                 # Make unique names for each dataframe so that they can be put into a single data frame at the end:
@@ -39,8 +26,25 @@ act.sbgr.bps.gis <- sbgr.BAP.max.sens %>%
                 new.names <- c(names(sbgr.hab.gis.2[,1]),str_c(act.code, orig.names, sep = "_")) 
                 new.names.2 <- str_replace(new.names,"[.]","_") # change points to underscores to make them database compatable
                 names(sbgr.hab.gis.2) <- new.names.2 # set the names to names 2
-                print(nrow(sbgr.hab.gis.2)) # error checking: if more than 804043 - there are duplicates records remaining in the dataframes - this is what needs fixing to complete it!
-                sbgr.hab.gis.2 #call the dataframe to ensure tha this is waht is saved in the end
+                print("Activity code: ",act.code," should have the same number of rows as the there are polygons in the habitat map. Check this value matches the expected number of rows", nrow(sbgr.hab.gis.2)) # error checking: if more than 804043 - there are duplicates records remaining in the dataframes - this is what needs fixing to complete it!
+                
+                
+                ##REMOVE columns with <NA> in name
+                #drop columns with NA for name (these may have arised in datasets where NA occured in the PressureCode column which were assigned NA if they were not present)
+                try(sbgr.hab.gis.2 <- sbgr.hab.gis.2 %>% select(-ends_with('<NA>'))) # this finds columns that ends with: ...<NA> and removes them
+                
+                #Add heat map values
+                heatmap.vls <- sbgr.hab.gis %>%
+                        dplyr::ungroup() %>%
+                        dplyr::group_by(pkey) %>%
+                        dplyr::summarise(heat.sum = sum(max.sens, na.rm=TRUE))
+                
+                #add activity code to names to mke it distinct for each activity
+                names(heatmap.vls[2]) <- paste0("heat_vl_",act.code)
+                
+                #bind heatmap values to pressure snesitivity table
+                sbgr.hab.gis.3 <- sbgr.hab.gis.2 %>%
+                        left_join(heatmap.vls, by = "pkey") #call the dataframe to ensure tha this is waht is saved in the end
                 
                 
                 
